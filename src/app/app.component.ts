9cd81a3638c5ae2941f726700b2b9c322928d143
Beatrice Daldin <beadaldin@gmail.com>
2024-06-07 16:51:48 +0200

    scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,21 +24,48 @@ export class AppComponent implements OnInit, OnDestroy {
         inputStream: {
           type: "LiveStream",
           constraints: {
-            width: 640,
-            height: 480,
+            width: 390,
+            height: 390,
             facingMode: "environment", // Utilizza la fotocamera posteriore
+          },
+          area: {
+            // defines rectangle of the detection/localization area
+            top: "0%", // top offset
+            right: "0%", // right offset
+            left: "0%", // left offset
+            bottom: "0%", // bottom offset
           },
         },
         decoder: {
           readers: [
-            "code_128_reader",
+            // "code_128_reader",
             "ean_reader",
-            "ean_8_reader",
-            "code_39_reader",
-            "upc_reader",
-            "upc_e_reader",
-            "code_93_reader",
+            // "ean_8_reader",
+            // "code_39_reader",
+            // "upc_reader",
+            // "upc_e_reader",
+            // "code_93_reader",
           ],
+          debug: {
+            drawBoundingBox: false,
+            showFrequency: false,
+            drawScanline: false,
+            showPattern: false,
+            showCanvas: false,
+            showPatches: false,
+            showFoundPatches: false,
+            showSkeleton: false,
+            showLabels: false,
+            showPatchLabels: false,
+            showRemainingPatchLabels: false,
+            boxFromPatches: {
+              showTransformed: false,
+              showTransformedBox: false,
+              showBB: false,
+            },
+          },
+          halfSample: true,
+          patchSize: "medium",
         },
       },
       (err) => {
@@ -54,7 +81,10 @@ export class AppComponent implements OnInit, OnDestroy {
       console.log("Codice a barre rilevato:", data.codeResult.code);
       // Puoi gestire il risultato qui
       const lastElement = this.scannedCodes[this.scannedCodes.length - 1];
-      if (lastElement !== data.codeResult.code) {
+      if (
+        lastElement !== data.codeResult.code &&
+        data.codeResult.format == "ean"
+      ) {
         this.scannedCodes.push(data.codeResult.code);
       }
     });
